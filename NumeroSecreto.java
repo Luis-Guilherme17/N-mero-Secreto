1ffd2546fc55d521f03ce372de28d1aba59e240a
Luis-Guilherme17 <lgluisguilherme1704@gmail.com>
2025-09-16 19:40:44 -0300

    NumeroSecreto.java

--- NumeroSecreto.java
+++ NumeroSecreto.java
@@ -8,32 +8,40 @@ public class NumeroSecreto {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
+
         
+        // Número secreto entre 1 e 10
         Random NumeroAleatorio = new Random();
         int NumeroSecreto = NumeroAleatorio.nextInt(1, 11);
         
         int tentativas = 0;
-        
+        int N;
         
     do{  
         System.out.println("Tente adivinhar o número secreto, digite de 1 a 10: ");
         N = scanner.nextInt();
         
         if(N > NumeroSecreto){
-           System.out.println("O número é menor que esse.");
-       }else {
-           System.out.println("O número digitado é maior que esse.");
-       }
-        
-        if(N != NumeroSecreto){
+            
+            System.out.println("O número é menor que esse.");
+            System.out.println("Número errado, tente novamente.");
+            
+        } else if (N < NumeroSecreto) {
+            
+            System.out.println("O número é maior que esse.");
             System.out.println("Número errado, tente novamente.");
         }
+        
         tentativas++;
         }while(N != NumeroSecreto);
             System.out.println("Número correto, parabéns!");
 
+        if (tentativas == 1) { 
+        System.out.println("Você acertou em " + tentativas + " tentativa.");
+        
+        } else if (tentativas > 1) {
         System.out.println("Você acertou em " + tentativas + " tentativas.");
+        }
        
         scanner.close();
     }
